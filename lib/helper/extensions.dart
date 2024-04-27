6d264192b448f0cce609a978524395f72c88a79c
Ahsan Raza <70075564+Ahsan668@users.noreply.github.com>
2024-04-28 02:51:45 +0500

    Update extensions.dart

--- lib/helper/extensions.dart
+++ lib/helper/extensions.dart
@@ -1,3 +1,4 @@
+//celcius-farenhiet-conversion
 extension StringCasingExtension on String {
   String toCapitalized() =>
       length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
